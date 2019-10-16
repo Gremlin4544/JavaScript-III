dfd1546b1b41dd923a1efc9ef1b4cdb8e1385aef
Gremlin4544 <alve@mckethen.com>
2019-10-16 15:40:23 -0400

    answered questions on this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window binding defaults the function to the object unless it is in strict mode which will stop the binding from breaking the code and will result in undefined
+* 2. Implicit binding is used 80% of the time in objects with methods and it refers to the object to the left of the dot "." when it is invoked.
+* 3. Explicit binding uses the .call, .bind and .apply tp pass arguments and will explicity state which keyword that this is being applied to i.e. .call(info). 
+* 4. New binding uses a new keyword in the constructor function to "construct" a new object and this will point to the new object.
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +13,16 @@
 
 // code example for Window Binding
 
+
 // Principle 2
 
 // code example for Implicit Binding
 
+
 // Principle 3
 
-// code example for New Binding
+// code example for Explicit Binding
 
 // Principle 4
 
-// code example for Explicit Binding
+// code example for New Binding
